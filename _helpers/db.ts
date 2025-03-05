19500d8767c9af00e6888266873c59d217e0f736
unknown <YY246@ccs.local>
2025-03-05 20:56:35 +0800

    FIRST

---
+++ _helpers/db.ts
@@ -0,0 +1,25 @@
+import config from './config.json'; // Changed to import
+import mysql from 'mysql2/promise';
+import { Sequelize } from 'sequelize';
+
+const db: any = {};
+
+initialize();
+
+async function initialize() {
+  // create db if it doesn't already exist
+  const { host, port, user, password, database } = config.database;
+  const connection = await mysql.createConnection({ host, port, user, password });
+  await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+
+  // connect to db
+  const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+
+  // init models and add them to the exported db object
+  db.User = require('../users/user.model')(sequelize);
+
+  // sync all models with database
+  await sequelize.sync({ alter: true });
+}
+
+export default db;
